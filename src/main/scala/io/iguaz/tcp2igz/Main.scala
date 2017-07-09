ffcf6c7ac4ea11b81b282c0bffb19ac644dea483
Gal Topper <galt@iguaz.io>
2017-07-09 12:19:02 +0300

    Replaced TCP client with a TCP server.

--- src/main/scala/io/iguaz/tcp2igz/Main.scala
+++ src/main/scala/io/iguaz/tcp2igz/Main.scala
@@ -2,8 +2,6 @@ package io.iguaz.tcp2igz
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
-import scala.concurrent.duration.Duration
-import scala.concurrent.{Await, Future}
 import scala.io.StdIn
 import scala.util.Try
 
@@ -12,9 +10,8 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
 import akka.http.scaladsl.model._
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Tcp.OutgoingConnection
-import akka.stream.scaladsl.{Flow, Sink, Source, Tcp}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Sink, Tcp}
+import akka.stream.{ActorMaterializer, FlowShape}
 import akka.util.ByteString
 import com.typesafe.config.{ConfigFactory, ConfigList}
 import play.api.libs.json.{JsObject, JsString, Json}
@@ -27,8 +24,8 @@ object Main {
     implicit val executionContext = system.dispatcher
     implicit val config = ConfigFactory.load()
 
-    val sourceHost = config.getString("source.host")
-    val sourcePort = config.getInt("source.port")
+    val host = config.getString("host")
+    val port = config.getInt("port")
 
     val targetHost = config.getString("target.host")
     val targetPort = config.getInt("target.port")
@@ -57,38 +54,40 @@ object Main {
       }
     }
 
-    val conversionFlow: Flow[ByteString, HttpRequest, NotUsed] = Flow[ByteString]
-      .statefulMapConcat(() => conversionFunction)
-      .map { values =>
-        val body = Json.obj(
-          "Key" -> Json.obj("id" -> Json.obj("S" -> JsString(values.head))),
-          "Item" -> JsObject(values.zip(fields).map { case (value, (name, igzType)) =>
-            name -> Json.obj(igzType -> JsString(value))
-          })
-        ).toString
+    Tcp().bind(host, port).runForeach { connection =>
+      println(s"New connection from: ${connection.remoteAddress}")
 
-        HttpRequest(
-          method = HttpMethods.PUT,
-          uri = uri,
-          headers = headers,
-          entity = HttpEntity(ContentTypes.`application/json`, body)
-        )
-      }
+      val conversionFlow: Flow[ByteString, HttpRequest, NotUsed] = Flow[ByteString]
+        .statefulMapConcat(() => conversionFunction)
+        .map { values =>
+          val body = Json.obj(
+            "Key" -> Json.obj("id" -> Json.obj("S" -> JsString(values.head))),
+            "Item" -> JsObject(values.zip(fields).map { case (value, (name, igzType)) =>
+              name -> Json.obj(igzType -> JsString(value))
+            })
+          ).toString
 
-    val bindingFuture: Future[OutgoingConnection] = Source.fromIterator(() => Iterator.continually(ByteString.empty))
-      .viaMat(Tcp().outgoingConnection(host = sourceHost, port = sourcePort)) { case (_, mat2) => mat2 }
-      .via(conversionFlow)
-      .via(clientFlow)
-      .via(printFlow)
-      .toMat(Sink.ignore) { case (mat1, _) => mat1 }
-      .run()
+          HttpRequest(
+            method = HttpMethods.PUT,
+            uri = uri,
+            headers = headers,
+            entity = HttpEntity(ContentTypes.`application/json`, body)
+          )
+        }
 
-    bindingFuture.failed.foreach(_ => system.terminate())
+      val flow = Flow.fromGraph(GraphDSL.create() { implicit b =>
+        import GraphDSL.Implicits._
+        val broadcast = b.add(new Broadcast[ByteString](2, eagerCancel = false))
+        broadcast.out(0) ~> conversionFlow ~> clientFlow ~> printFlow ~> Sink.ignore
+        FlowShape(broadcast.in, broadcast.out(1))
+      })
 
-    Await.result(bindingFuture, Duration.Inf)
+      connection.handleWith(flow)
+    }
 
-    println(s"Reading TCP data from $sourceHost:$sourcePort\nType q and then RETURN to stop...")
+    println(s"Accepting TCP connections at $host:$port\nType q and then RETURN to stop...")
     while (StdIn.readLine() != "q") {} // let it run until user exits
+    println("Shutting down! This may take some seconds.")
     system.terminate()
   }
 
